7f7b3418cedd5ea4a79612028c88139565c9b751
Daniel Crumpler <dancrumpy@gmail.com>
2020-10-22 10:06:48 -0400

    Commit #1
    Initial Project

---
+++ src/edu/westga/cs3152/model/Main.java
@@ -0,0 +1,14 @@
+package edu.westga.cs3152.model;
+
+/**
+ * The Class Main
+ * 
+ * @author Daniel Crumpler
+ * @version Fall 2020
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+}
